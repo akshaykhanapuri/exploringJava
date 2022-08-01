512b476072c7fd3d94cf149891b4483fc204df61
Akshay <akhanapuri@twilio.com>
2022-08-01 16:21:24 +0530

    Input and output in Java

--- CommunityClassroom/src/com/Akshay/Main.java
+++ CommunityClassroom/src/com/Akshay/Main.java
@@ -1,7 +1,9 @@
 package com.Akshay;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello Akshay");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
