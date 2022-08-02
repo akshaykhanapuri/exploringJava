ca72ae320f6f3a1bc52c377cefdcc9be243fdbda
Akshay <akhanapuri@twilio.com>
2022-08-02 14:55:54 +0530

    added loops

---
+++ ConditionalsAndLoops/src/com/Akshay/Loops.java
@@ -0,0 +1,25 @@
+package com.Akshay;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        //for loop
+        for(int num = 1; num <=5; num++){
+            System.out.println(num);
+        }
+
+        //while loop
+        int num = 1;
+        while(num <= 5){
+            System.out.println(num);
+            num++;
+        }
+
+        //do while
+        num = 1;
+        do{
+            System.out.println(num);
+            num++;
+        }while(num <= 1);
+    }
+}
